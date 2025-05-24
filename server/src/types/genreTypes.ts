c9da02866109e2c08d0e5c424de088335592ad63
Al Moreno <kevapaereo@gmail.com>
2025-05-24 23:32:17 +0200

    Expanded Types

---
+++ server/src/types/genreTypes.ts
@@ -0,0 +1,106 @@
+enum GenreBase {
+  Action = 'Action',
+  Adventure = 'Adventure',
+  Comedy = 'Comedy',
+  Drama = 'Drama',
+  Fantasy = 'Fantasy',
+  Horror = 'Horror',
+  Mystery = 'Mystery',
+  Romance = 'Romance',
+  SciFi = 'Science Fiction',
+  Thriller = 'Thriller',
+  Crime = 'Crime',
+  Historical = 'Historical',
+  War = 'War',
+  Western = 'Western',
+  Family = 'Family',
+}
+
+enum AudiovisualGenre {
+  Animation = 'Animation',
+  Documentary = 'Documentary',
+  Musical = 'Musical',
+  Biography = 'Biography',
+  Sport = 'Sport',
+  FilmNoir = 'Film Noir',
+  Experimental = 'Experimental',
+}
+
+enum ShowFormat {
+  Series = 'Series',
+  MiniSeries = 'Mini Series',
+  Anthology = 'Anthology Series',
+  RealityShow = 'Reality Show',
+  TalkShow = 'Talk Show',
+  GameShow = 'Game Show',
+  NewsShow = 'News Show',
+  VarietyShow = 'Variety Show',
+  Documentary = 'Documentary Series',
+  WebSeries = 'Web Series',
+}
+
+enum GameplayGenre {
+  Platform = 'Platform',
+  Shooter = 'Shooter',
+  Fighting = 'Fighting',
+  BeatEmUp = "Beat 'em up",
+  Stealth = 'Stealth',
+  Survival = 'Survival',
+  Rhythm = 'Rhythm',
+
+  Strategy = 'Strategy',
+  RealTimeStrategy = 'Real-Time Strategy',
+  TurnBased = 'Turn-Based',
+  TowerDefense = 'Tower Defense',
+
+  RPG = 'Role-Playing Game',
+  ActionRPG = 'Action RPG',
+  MMORPG = 'Massively Multiplayer Online RPG',
+  TacticalRPG = 'Tactical RPG',
+
+  VisualNovel = 'Visual Novel',
+  AdventurePoint = 'Point & Click Adventure',
+  InteractiveFiction = 'Interactive Fiction',
+
+  Simulation = 'Simulation',
+  LifeSim = 'Life Simulation',
+  VehicleSim = 'Vehicle Simulation',
+  CitySim = 'City Building',
+  BusinessSim = 'Business Simulation',
+  SportSim = 'Sports Simulation',
+
+  Puzzle = 'Puzzle',
+  Board = 'Board Game',
+  Educational = 'Educational',
+  Trivia = 'Trivia',
+
+  OpenWorld = 'Open World',
+  Sandbox = 'Sandbox',
+  Battle = 'Battle Royale',
+  MOBA = 'Multiplayer Online Battle Arena',
+  Roguelike = 'Roguelike',
+  Metroidvania = 'Metroidvania',
+}
+
+enum GameTheme {
+  Medieval = 'Medieval',
+  Cyberpunk = 'Cyberpunk',
+  PostApocalyptic = 'Post-Apocalyptic',
+  Space = 'Space',
+  Military = 'Military',
+  Superhero = 'Superhero',
+  Horror = 'Horror',
+  Fantasy = 'Fantasy',
+  Historical = 'Historical',
+  Contemporary = 'Contemporary',
+  Steampunk = 'Steampunk',
+  Anime = 'Anime',
+  Cartoon = 'Cartoon',
+  Realistic = 'Realistic',
+}
+
+type FilmGenre = GenreBase | AudiovisualGenre;
+type ShowGenre = GenreBase | AudiovisualGenre | ShowFormat;
+type GameGenre = GenreBase | GameTheme;
+
+export { FilmGenre, ShowGenre, GameGenre, GameplayGenre };
--- server/src/types/mediaTypes.ts
+++ server/src/types/mediaTypes.ts
@@ -1,22 +1,54 @@
 import Country from './countryTypes';
+import { FilmGenre, ShowGenre, GameGenre, GameplayGenre } from './genreTypes';
 
 enum MediaType {
-  'Film',
-  'Show',
-  'Game',
+  Film = 'Film',
+  Show = 'Show',
+  Game = 'Game',
 }
 
 enum SubMediaType {
-  'Season',
-  'Chapter',
-  'DLC',
+  None = 'None',
+  Season = 'Season',
+  Chapter = 'Chapter',
+  DLC = 'DLC',
 }
 
 enum AuthorType {
-  'Director',
-  'Writer',
-  'Actor',
+  Director = 'Director',
+  Writer = 'Writer',
+  Actor = 'Actor',
 }
+
+enum FilmParental {
+  G = 'General Audience',
+  PG = 'Parental Guidance Suggested',
+  PG13 = 'Parental Guidance for Children Under 13',
+  R = 'Restricted',
+  NC17 = 'Adults Only',
+}
+
+enum ShowParental {
+  TVY = 'All Children',
+  TVY7 = 'Directed to Older Children Age 7 and Above',
+  TVY7FV = 'Directed to Older Children - Fantasy Violence',
+  TVG = 'General Audience',
+  TVPG = 'Parental Guidance Suggested',
+  TV14 = 'Parents Strongly Cautioned',
+  TVMA = 'Mature Audience',
+}
+
+enum GameParental {
+  EC = 'Early Childhood',
+  E = 'Everyone',
+  E10 = 'Everyone 10+',
+  T = 'Teen',
+  M = 'Mature 17+',
+  AO = 'Adults Only 18+',
+  RP = 'Rating Pending',
+}
+
+type ParentalGuide = GameParental | FilmParental | ShowParental;
 
 interface Individual {
   id: number;
@@ -30,6 +62,7 @@ interface Creator extends Individual {
 
 interface Author extends Creator {
   type: AuthorType;
+  birthYear: number;
 }
 
 interface Director extends Author {
@@ -42,26 +75,82 @@ interface Writer extends Author {
 interface Actor extends Author {
   type: AuthorType.Actor;
 }
-
+interface Character extends Individual {
+  actor: Author;
+}
 interface Studio extends Individual {}
 
 interface Media {
   id: number;
   name: string;
+  originalName: string;
   sortname: string;
-  year: number;
+  description: string;
+  parentalGuide: ParentalGuide;
+  releaseDate: Date;
   image: string;
   rating: number;
   type: MediaType;
+  genres: GameGenre[] | FilmGenre[] | ShowGenre[];
+  subMedia: SubMediaType;
   studios: Studio[];
   directors: Director[];
   writers: Writer[];
   countries: Country[];
+  cast: Character[];
 }
 
 interface Film extends Media {
   type: MediaType.Film;
+  subMedia: SubMediaType.None;
+  parentalGuide: FilmParental;
+  genres: FilmGenre[];
+}
+interface Show extends Media {
+  type: MediaType.Show;
   subMedia: SubMediaType.Season;
+  seasons: Season[];
+  genres: ShowGenre[];
+}
+interface Game extends Media {
+  type: MediaType.Game;
+  subMedia: SubMediaType.Chapter | SubMediaType.DLC;
+  parentalGuide: GameParental;
+  dlc: DLC[];
+  chapters: Chapter[];
+  genres: GameGenre[];
+  gamePlayGenres: GameplayGenre[];
 }
 
-export { Media, Author, MediaType, Film, Actor };
+interface SubMedia extends Media {
+  subType: SubMediaType;
+  parentId: number;
+}
+interface Season extends SubMedia {
+  type: MediaType.Show;
+  subType: SubMediaType.Season;
+}
+
+interface DLC extends SubMedia {
+  type: MediaType.Game;
+  subType: SubMediaType.DLC;
+}
+
+interface Chapter extends SubMedia {
+  type: MediaType.Game;
+  subType: SubMediaType.Chapter;
+}
+
+export {
+  Media,
+  Author,
+  MediaType,
+  Film,
+  Actor,
+  Studio,
+  Show,
+  Season,
+  Game,
+  DLC,
+  Chapter,
+};
