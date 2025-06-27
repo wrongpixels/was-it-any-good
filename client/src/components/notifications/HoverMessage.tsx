e3afaf2b96351e51d1939c8a25d3ef5b4d51b189
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-06-27 14:23:14 +0200

    Fixed hover message

--- client/src/components/notifications/HoverMessage.tsx
+++ client/src/components/notifications/HoverMessage.tsx
@@ -1,7 +1,7 @@
 import { JSX } from 'react';
 
 const DEF_CLASSNAME: string =
-  'bg-hoverblue/80 shadow-sm text-white font-bold border-2 border-cyan-900 rounded-md text-2xl -translate-y-19 min-w-12 min-h-10 ring-1 px-2 ring-gray-500 text';
+  'bg-hoverblue/80 shadow-sm text-white font-bold border-2 border-cyan-900 rounded-md text-2xl -translate-y-20 min-w-13 min-h-12 ring-1 px-2 ring-gray-500 text';
 
 interface HoverMessageProps {
   message: string;
@@ -17,8 +17,17 @@ const HoverMessage = ({
   if (!message || message === '0') {
     return null;
   }
+  if (message === 'Unvote') {
+    extraClassname = 'font-semibold text-xl';
+  }
 
-  return <div className={`${classname} ${extraClassname}`}>{message}</div>;
+  return (
+    <div
+      className={`flex items-center justify-center ${classname} ${extraClassname}`}
+    >
+      {message}
+    </div>
+  );
 };
 
 export default HoverMessage;
