84d92e7e0a5badeb35e27a4c7bb2152014b8f168
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-06-27 15:21:58 +0200

    Averages on shows

--- client/src/components/MediaPoster.tsx
+++ client/src/components/MediaPoster.tsx
@@ -1,12 +1,14 @@
 import { JSX } from 'react';
 import { MediaResponse } from '../../../shared/types/models';
 import StarRating from './Rating/StarRating';
+import { calculateAverage } from '../utils/ratings-helper';
 
 interface MediaPosterProps {
   media: MediaResponse;
 }
 
 const MediaPoster = ({ media }: MediaPosterProps): JSX.Element => {
+  console.log(calculateAverage(media));
   return (
     <div>
       <div className="bg-white shadow-md rounded border-9 border-white ring-1 ring-gray-300 self-start">
@@ -19,7 +21,7 @@ const MediaPoster = ({ media }: MediaPosterProps): JSX.Element => {
         />
         <div className="text-center">
           <StarRating
-            rating={media.baseRating}
+            rating={calculateAverage(media)}
             media={media}
             valid={true}
             mediaType={media.mediaType}
--- client/src/utils/ratings-helper.ts
+++ client/src/utils/ratings-helper.ts
@@ -1,5 +1,41 @@
 import { UserVote } from '../../../shared/types/common';
+import { MediaType } from '../../../shared/types/media';
+import { MediaResponse } from '../../../shared/types/models';
 
 export const numToVote = (num: number): UserVote => {
   return Math.max(-1, Math.min(num, 10)) as UserVote;
+};
+
+//To create a weighted average for the 'show' as a whole, taking all seasons and
+//their individual ratings into consideration
+export const calculateAverage = (media: MediaResponse): number => {
+  const globalAverage: number =
+    media.rating !== null && media.rating > 0 ? media.rating : media.baseRating;
+
+  if (media.mediaType !== MediaType.Show) {
+    return globalAverage;
+  }
+
+  const SHOW_WEIGHT = 0.4;
+  const SEASONS_WEIGHT = 0.6;
+
+  if (!media.seasons || media.seasonCount === 0) {
+    return globalAverage;
+  }
+
+  const seasonsAverage =
+    media.seasons.reduce(
+      (sum, season) =>
+        sum +
+        ((season.rating !== null && season.rating > 0
+          ? Number(season.rating)
+          : Number(season.baseRating)) || 0),
+      0
+    ) / media.seasonCount;
+  //We round the result to only 1 decimal
+  return seasonsAverage > 0
+    ? Math.round(
+        (globalAverage * SHOW_WEIGHT + seasonsAverage * SEASONS_WEIGHT) * 10
+      ) / 10
+    : globalAverage;
 };
