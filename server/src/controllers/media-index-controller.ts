0545e3ec7be0363918b86a273cfda4c6aea80738
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-07-09 15:38:52 +0200

    media index controller

---
+++ server/src/controllers/media-index-controller.ts
@@ -0,0 +1,50 @@
+import express, { NextFunction, Response } from 'express';
+import path from 'path';
+import fs from 'fs/promises';
+import { tmdbAPI } from '../util/config';
+import { MediaType } from '../../../shared/types/media';
+
+const router = express.Router();
+const PAGES_TO_GATHER: number = 10;
+const DB_PATH: string = path.join(__dirname, '../db');
+const gatherMedia = async (mediaType: MediaType): Promise<number> => {
+  const media: unknown[] = [];
+
+  for (let page = 1; page <= PAGES_TO_GATHER; page++) {
+    const response = await tmdbAPI.get(
+      `/discover/${mediaType === MediaType.Film ? 'movie' : 'tv'}`,
+      {
+        params: {
+          include_adult: false,
+          include_video: false,
+          language: 'en-US',
+          page,
+          sort_by: 'popularity.desc',
+        },
+      }
+    );
+    media.push(...response.data.results);
+  }
+
+  await fs.mkdir(DB_PATH, { recursive: true });
+  await fs.writeFile(
+    path.join(
+      DB_PATH,
+      `popular-${mediaType === MediaType.Film ? 'films' : 'shows'}-db.json`
+    ),
+    JSON.stringify(media, null, 2)
+  );
+  return media.length;
+};
+
+router.post('/populate', async (_req, res: Response, next: NextFunction) => {
+  try {
+    const films: number = await gatherMedia(MediaType.Film);
+    const shows: number = await gatherMedia(MediaType.Show);
+    res.json({ shows, films });
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
