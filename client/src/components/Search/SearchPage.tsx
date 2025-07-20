8c92e3335e61f0928a8f546b26ea7b0463fb0fd4
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-07-20 22:41:00 +0200

    Base Search page with auto-navigate

--- client/src/components/Search/SearchPage.tsx
+++ client/src/components/Search/SearchPage.tsx
@@ -1,11 +1,19 @@
-import { SetURLSearchParams, useSearchParams } from 'react-router-dom';
+import {
+  SetURLSearchParams,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
 import { useInputField } from '../../hooks/use-inputfield';
 import { InputField } from '../common/InputField';
-import { JSX } from 'react';
+import { FormEvent, JSX } from 'react';
 import { setPageInfo } from '../../utils/page-info-setter';
 import SearchIcon from '../Header/Search/icons/SearchIcon';
+import { useSuggestionsQuery } from '../../queries/suggestions-queries';
+import SpinnerPage from '../common/status/SpinnerPage';
+import { routerPaths } from '../../utils/url-helper';
 
 const SearchPage = (): JSX.Element | null => {
+  const navigateTo = useNavigate();
   const [parameters]: [URLSearchParams, SetURLSearchParams] = useSearchParams();
   const searchTerm: string | null = parameters.get('q');
   const searchField = useInputField({
@@ -13,19 +21,37 @@ const SearchPage = (): JSX.Element | null => {
     initialValue: searchTerm || undefined,
     placeholder: 'Search on WIAG and TMDB',
   });
+  const { data: suggestions, isLoading } = useSuggestionsQuery(
+    searchTerm || ''
+  );
   setPageInfo({ title: `${searchTerm ? `${searchTerm} - ` : ''}Search` });
+  const handleOnSearch = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigateTo(routerPaths.search.byTerm(searchField.value));
+  };
+
   return (
-    <div className="flex justify-center flex-row">
+    <div className="flex flex-col items-center">
       <div className="relative">
         <SearchIcon
           className="absolute left-0.5 top-1/2 -translate-y-1/2 text-gray-400 mt-0.5"
           sizePadding={5}
         />
-        <InputField
-          {...searchField.getProps()}
-          className="border pl-7 text-base py-1.5 w-80 shadow-md shadow-black/5"
-        />
+        <form onSubmit={handleOnSearch}>
+          <InputField
+            {...searchField.getProps()}
+            className="border pl-7 text-base py-1.5 w-80 shadow-md shadow-black/5"
+          />
+        </form>
       </div>
+      <span className="pt-5">
+        {!!suggestions && (
+          <span className="font-medium">
+            {suggestions.length || 'No'} results!
+          </span>
+        )}
+        {isLoading && <SpinnerPage text={`Searching for "${searchTerm}"...`} />}
+      </span>
     </div>
   );
 };
