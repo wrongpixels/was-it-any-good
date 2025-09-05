f8a3951c47c7cc115b4fa1c99dc8052c4887970c
Al Moreno <kevapaereo@gmail.com>
2025-09-05 21:56:42 +0100

    Loading Cards

--- client/src/components/Search/Loading/LoadingSearch.tsx
+++ client/src/components/Search/Loading/LoadingSearch.tsx
@@ -1,9 +1,6 @@
 import { OptClassNameProps } from '../../../types/common-props-types';
 import { mergeClassnames } from '../../../utils/lib/tw-classname-merger';
 
-const cardClassName =
-  'bg-gradient-to-t from-gray-300 to-gray-300 via-gray-300/70 w-77.5 rounded h-48 shadow animate-pulse';
-
 interface LoadingCardsProps extends OptClassNameProps {
   placeholderCount?: number;
 }
@@ -12,13 +9,34 @@ const LoadingCards = ({
   placeholderCount = 21,
   className,
 }: LoadingCardsProps) => {
-  const gridCols = 'grid grid-cols-3 gap-4';
+  const gridClassName = 'grid grid-cols-3 gap-4';
+
   return (
-    <div className={mergeClassnames(gridCols, className)}>
+    <div className={mergeClassnames(gridClassName, className)}>
       {[...Array(placeholderCount)].map((_, i) => (
-        <span key={i} className={cardClassName} />
+        <LoadingCard key={i} />
       ))}
     </div>
+  );
+};
+export const LoadingCard = () => {
+  const cardClassName =
+    'relative h-48 w-77.5 animate-pulse rounded bg-gradient-to-t from-gray-300 to-gray-300 via-gray-300/70 shadow';
+  return (
+    <div className={cardClassName}>
+      <LoadingCardContent />
+    </div>
+  );
+};
+const LoadingCardContent = () => {
+  return (
+    <>
+      <div className="absolute left-2 top-2 h-44 w-29.5 rounded-lg bg-gray-400/50" />
+      <div className="absolute left-35 top-5 h-5 w-30 rounded-lg bg-gray-400/50" />
+      <div className="absolute left-35 top-12 h-5 w-20 rounded-lg bg-gray-400/50" />
+      <div className="absolute left-50.5 top-31.5 h-7 w-9 rounded-lg bg-gray-400/50" />
+      <div className="absolute left-42 top-40.5 h-5 w-26 rounded-lg bg-gray-400/50" />
+    </>
   );
 };
 
