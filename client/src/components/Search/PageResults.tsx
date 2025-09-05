96c3ed8cb22a713f78ab509bdc28035917931fce
Al Moreno <kevapaereo@gmail.com>
2025-09-05 11:53:29 +0100

    Refactored PageResults

--- client/src/components/Search/PageResults.tsx
+++ client/src/components/Search/PageResults.tsx
@@ -13,21 +13,10 @@ import {
   OverrideParams,
   URLParameters,
 } from '../../types/search-browse-types';
-import {
-  invertSortDir,
-  isSortBy,
-  SortBy,
-  sortByValues,
-  SortDir,
-} from '../../../../shared/types/browse';
-import Dropdown from '../common/Dropdown';
-import useDropdown from '../../hooks/use-dropdown';
-import { queryTypeToDisplayName } from '../../utils/url-helper';
 import { NavigateToQueryOptions } from '../../hooks/use-url-query-manager';
-import Button from '../common/Button';
-import IconInvertSortDir from '../common/icons/sorting/IconInvertSortDir';
-import { styles } from '../../constants/tailwind-styles';
 import SpinnerPage from '../common/status/SpinnerPage';
+import PageResultsSort from './PageResultsSort';
+import PageResultsTitle from './PageResultsTitle';
 
 interface PageResultsProps {
   results: IndexMediaResponse | undefined;
@@ -38,7 +27,7 @@ interface PageResultsProps {
   title?: string;
   badgeType: BadgeType;
   showNavBar?: boolean;
-  showOrderOptions?: boolean;
+  showSortOptions?: boolean;
   isLoading?: boolean;
 }
 //we render here the results, shared between Search and Browse
@@ -51,7 +40,7 @@ const PageResults = ({
   isLoading,
   badgeType = BadgeType.None,
   showNavBar = true,
-  showOrderOptions = true,
+  showSortOptions = true,
 }: PageResultsProps): JSX.Element | null => {
   if (!results) {
     return null;
@@ -60,29 +49,8 @@ const PageResults = ({
     navigateToQuery({ replace: true, overrideParams });
   };
 
-  const applySorByFilter = (newValue: string) =>
-    submitFilter({
-      sortBy: isSortBy(newValue) ? newValue : SortBy.Popularity,
-    });
-
-  const invertedSortDir: boolean = urlParams.sortDir === SortDir.Inverted;
-  const toggleSortDirFilter = () =>
-    submitFilter({
-      sortDir: invertSortDir(urlParams.sortDir),
-    });
-  const orderDropdown = useDropdown({
-    name: 'sortBy',
-    defaultValue: urlParams.sortBy || SortBy.Popularity,
-    onChanged: applySorByFilter,
-  });
-
-  const searchTerm = (): JSX.Element => (
-    <>
-      {' for '}
-      <span className="italic text-gray-500 font-normal">"{term}"</span>
-    </>
-  );
   const indexOffset: number = (results.page - 1) * PAGE_LENGTH + 1;
+
   //we memo the list of result Cards for performance
   const resultEntries: JSX.Element[] = useMemo(
     () =>
@@ -101,33 +69,18 @@ const PageResults = ({
       {showNavBar && (
         <>
           <div className="relative w-full h-8 flex flex-row">
-            <span className="w-full text-center text-lg">
-              {results.totalResults || 'No'}
-              {` ${queryTypeToDisplayName(urlParams.queryType)} `}
-              {' results '}
-              <>{term && searchTerm()}</>
-            </span>
+            <PageResultsTitle
+              totalResults={results.totalResults}
+              queryType={urlParams.queryType}
+              term={term}
+            />
             <PageResultsNav results={results} navigatePages={navigatePages} />
           </div>
-          {showOrderOptions && (
-            <div className="absolute -translate-y-1 flex flex-row gap-2 ">
-              <Dropdown
-                {...orderDropdown.getProps()}
-                options={sortByValues}
-                label="Sort by"
-              />
-              <Button
-                title={
-                  invertedSortDir ? 'Set default order' : 'Invert default order'
-                }
-                variant="dropdown"
-                onClick={() => toggleSortDirFilter()}
-                className={`p-0 px-1 h-9.25 ${invertedSortDir ? `${styles.buttons.blue}` : ''}`}
-              >
-                <IconInvertSortDir width={16} />
-              </Button>
-            </div>
-          )}
+          <PageResultsSort
+            showSortOptions={showSortOptions}
+            urlParams={urlParams}
+            submitFilter={submitFilter}
+          />
         </>
       )}
       {(isLoading && (
