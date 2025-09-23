5a80fa0ea1c1072cb0604b285f448cb3accd6a89
Al Moreno <kevapaereo@gmail.com>
2025-09-23 15:07:34 +0100

    No retries on testing vitest query client

--- client/src/test/test-utils.ts
+++ client/src/test/test-utils.ts
@@ -3,7 +3,11 @@ import { QueryClient } from '@tanstack/react-query';
 let queryClient: QueryClient | undefined = undefined;
 
 export const createQueryClient = (): QueryClient => {
-  queryClient = new QueryClient();
+  queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { retry: false },
+    },
+  });
   return queryClient;
 };
 
