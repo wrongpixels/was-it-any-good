487558d6776caef164a03da7ac51a551d642e965
Al Moreno <kevapaereo@gmail.com>
2025-10-03 18:46:33 +0100

    User Media Lists Ready

---
+++ server/src/models/users/userMediaListItem.ts
@@ -0,0 +1,89 @@
+import {
+  CreationOptional,
+  DataTypes,
+  InferAttributes,
+  InferCreationAttributes,
+  Model,
+} from 'sequelize';
+import { IndexMediaData } from '../../../../shared/types/models';
+import UserMediaList from './userMediaList';
+import IndexMedia from '../media/indexMedia';
+import { sequelize } from '../../util/db/initialize-db';
+import { MAX_ITEMS_LIST_INDEX } from '../../../../shared/constants/user-media-list-constants';
+
+class UserMediaListItem extends Model<
+  InferAttributes<UserMediaListItem>,
+  InferCreationAttributes<UserMediaListItem>
+> {
+  declare id: CreationOptional<number>;
+  declare indexInList: number;
+  declare userListId: number;
+  declare userList?: UserMediaList;
+  declare indexId: number;
+  declare indexMedia?: IndexMediaData;
+  declare createdAt: CreationOptional<Date>;
+  declare updatedAt: CreationOptional<Date>;
+
+  static associate() {
+    this.belongsTo(UserMediaList, {
+      foreignKey: 'userListId',
+      as: 'userList',
+    });
+    this.belongsTo(IndexMedia, { foreignKey: 'indexId', as: 'indexMedia' });
+  }
+}
+
+UserMediaListItem.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    indexInList: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+
+      validate: {
+        min: 0,
+        max: MAX_ITEMS_LIST_INDEX - 1, // so if MAX = 100, positions can only be 0–99.
+        //we always control the max size via service logic, since sequelize associations
+        //can't directly enforce a "max children" constraint
+      },
+    },
+    userListId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    indexId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    modelName: 'user_media_list_item',
+    indexes: [
+      {
+        unique: true,
+        fields: ['index_id', 'user_list_id'],
+      },
+      {
+        unique: true,
+        fields: ['user_list_id', 'index_in_list'],
+      },
+    ],
+  }
+);
+
+export default UserMediaListItem;
