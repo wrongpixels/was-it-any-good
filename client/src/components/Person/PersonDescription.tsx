73da29a9d3af192bbc87fb8559325b50af27495a
Al Moreno <kevapaereo@gmail.com>
2025-10-07 20:40:59 +0100

    Added Person descriptions with 2 sentence limit. Added sentence-splitter

---
+++ client/src/components/Person/PersonDescription.tsx
@@ -0,0 +1,38 @@
+import { JSX, memo } from 'react';
+import { OptStringProps } from '../../types/common-props-types';
+import { mergeClassnames } from '../../utils/lib/tw-classname-merger';
+import { CLEAN_WIKIPEDIA_DESCRIPTION } from '../../constants/format-constants';
+import { split } from 'sentence-splitter';
+import { TxtParentNodeWithSentenceNodeContent } from 'sentence-splitter';
+
+const baseClassName: string = 'text-sm text-gray-400 italic';
+
+//we remove the Wikipedia sentence from the description.
+export const cleanDescription = (text: string): string => {
+  const nodes: TxtParentNodeWithSentenceNodeContent[] = split(text);
+
+  const sentences: string[] = nodes
+    .filter((node) => node.type === 'Sentence')
+    .map((node) => node.raw.trim());
+
+  return sentences
+    .filter((s) => !s.includes(CLEAN_WIKIPEDIA_DESCRIPTION))
+    .slice(0, 2)
+    .join(' ')
+    .trim();
+};
+const PersonDescription = ({
+  text,
+  className,
+}: OptStringProps): JSX.Element | null => {
+  if (!text) {
+    return null;
+  }
+  return (
+    <div className={mergeClassnames(baseClassName, className)}>
+      {cleanDescription(text)}
+    </div>
+  );
+};
+
+export default memo(PersonDescription);
