e93cd78c5a6e5b1c1c1e8a1791113e02e99facbc
Al Moreno <kevapaereo@gmail.com>
2025-10-08 16:46:08 +0100

    Adding UserMediaList interface

---
+++ client/src/components/Common/Icons/Lists/IconWatchlistRemove.tsx
@@ -0,0 +1,28 @@
+import { OptIconProps } from '../../../../types/common-props-types';
+import SVG from '../../Custom/SVG';
+
+const IconWatchlistRemove = ({
+  width,
+  height,
+  url,
+  newTab,
+  ...rest
+}: OptIconProps) => (
+  <span {...rest}>
+    <SVG
+      url={url}
+      newTab={newTab}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width={width}
+      height={height}
+    >
+      <path
+        fill="currentColor"
+        d="M7 1C5.34315 1 4 2.34315 4 4V20.9425C4 22.6114 5.92338 23.5462 7.23564 22.5152L12 18.7717L16.7644 22.5152C18.0766 23.5462 20 22.6114 20 20.9425V4C20 2.34315 18.6569 1 17 1H7Z"
+      />
+    </SVG>
+  </span>
+);
+
+export default IconWatchlistRemove;
--- client/src/components/Media/MediaPage.tsx
+++ client/src/components/Media/MediaPage.tsx
@@ -31,6 +31,7 @@ import { isShow } from '../../utils/ratings-helper';
 import WrongIdFormatPage from '../Common/Status/WrongIdFormatPage';
 import { isNotFoundError } from '../../utils/error-handler';
 import CreatingMediaPage from '../Common/Status/CreatingMediaPage';
+import UserLists from '../UserLists/UserLists';
 
 interface MediaPage {
   mediaType: MediaType;
@@ -141,9 +142,10 @@ const MediaPage = ({
             )}
           </div>
         </span>
-        <span className="w-50 hidden md:block">
+        <div className="flex-col w-50 hidden md:flex gap-3">
           <MediaPagePoster media={media} />
-        </span>
+          <UserLists />
+        </div>
       </div>
       {media.mediaType === MediaType.Show && <SeasonsSection show={media} />}
       <div className="mt-4 border-t border-gray-200">
