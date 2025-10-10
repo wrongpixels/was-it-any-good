2bfb83f06b714eb2b22e90bffc51ae99f81f051f
Al Moreno <kevapaereo@gmail.com>
2025-10-10 12:26:43 +0100

    Added PersonDetailsBuilder for auto details handling in person pages + auto description generator when default is empty.

--- client/src/components/Person/PersonDescription.tsx
+++ client/src/components/Person/PersonDescription.tsx
@@ -1,35 +1,12 @@
 import { JSX, memo } from 'react';
 import { OptStringProps } from '../../types/common-props-types';
 import { mergeClassnames } from '../../utils/lib/tw-classname-merger';
-import {
-  CLEAN_WIKIPEDIA_DESCRIPTION,
-  CLEAN_WIKIPEDIA_PARTIAL_DESCRIPTION,
-} from '../../constants/format-constants';
-import { split } from 'sentence-splitter';
-import { TxtParentNodeWithSentenceNodeContent } from 'sentence-splitter';
-import { toFirstUpperCase } from '../../../../shared/helpers/format-helper';
+
+//a component that shows a description for People, either by formatting and
+//adapting the TMDB biography or building our own with the data we have
 
 const baseClassName: string = 'text-sm text-gray-400 italic';
 
-//we remove the Wikipedia sentence from the description and only show 2 sentences max.
-export const cleanDescription = (text: string): string => {
-  const nodes: TxtParentNodeWithSentenceNodeContent[] = split(text);
-
-  const sentences: string[] = nodes
-    .filter(
-      (node: TxtParentNodeWithSentenceNodeContent) => node.type === 'Sentence'
-    )
-    .map((node: TxtParentNodeWithSentenceNodeContent) => node.raw.trim());
-
-  return sentences
-    .filter((s: string) => !s.includes(CLEAN_WIKIPEDIA_DESCRIPTION))
-    .slice(0, 2)
-    .map((s: string) =>
-      s.replace(/\u00A0/g, ' ').replace(CLEAN_WIKIPEDIA_PARTIAL_DESCRIPTION, '')
-    )
-    .join(' ')
-    .trim();
-};
 const PersonDescription = ({
   text,
   className,
@@ -38,9 +15,7 @@ const PersonDescription = ({
     return null;
   }
   return (
-    <div className={mergeClassnames(baseClassName, className)}>
-      {toFirstUpperCase(cleanDescription(text))}
-    </div>
+    <div className={mergeClassnames(baseClassName, className)}>{text}</div>
   );
 };
 
