d15ef1ce662e5aac57b4e03dd697ee447dc534eb
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-12-13 19:51:13 +0100

    Increased timeout

--- server/src/util/db/initialize-db.ts
+++ server/src/util/db/initialize-db.ts
@@ -13,8 +13,8 @@ const sequelize = new Sequelize(POSTGRES_URI, {
     evict: 1000,
   },
   dialectOptions: {
-    statement_timeout: 2500,
-    idle_in_transaction_session_timeout: 3000,
+    statement_timeout: 10000,
+    idle_in_transaction_session_timeout: 15000,
   },
 });
 
