de807205679d47722c0e85461c3c05cc5042f0f0
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-12-24 19:39:48 +0100

    Added options to review specific seasons

--- client/src/components/UserReviews/UserReviewForm.tsx
+++ client/src/components/UserReviews/UserReviewForm.tsx
@@ -1,18 +1,39 @@
-import { JSX, useState } from 'react';
+import { JSX, PropsWithChildren, useState } from 'react';
 import { useInputField } from '../../hooks/use-inputfield';
 import { InputField } from '../Common/Custom/InputField';
+import Button from '../Common/Custom/Button';
+import IconCreate from '../Common/Icons/IconCreate';
+import { AnimatedDiv } from '../Common/Custom/AnimatedDiv';
+import {
+  MediaResponse,
+  SeasonResponse,
+  ShowResponse,
+} from '../../../../shared/types/models';
+import { mediaTypeToDisplayName } from '../../utils/url-helper';
+import Dropdown from '../Common/Custom/Dropdown';
+import { isShow } from '../../../../shared/helpers/media-helper';
+import { getVisibleSeasons } from '../../utils/seasons-setter';
 
 const SHOW_REVIEW_FORM: boolean = false;
 
-const UserReviewForm = (): JSX.Element | null => {
+interface UserReviewFormProps {
+  media: MediaResponse | ShowResponse;
+}
+
+const UserReviewForm = ({ media }: UserReviewFormProps): JSX.Element | null => {
   if (!SHOW_REVIEW_FORM) {
     return null;
   }
+  const seasonNames: string[] =
+    isShow(media) && media.seasons
+      ? getVisibleSeasons(media.seasons).map((s: SeasonResponse) => s.name)
+      : [];
   const [mainTextContent, setMainTextContent] = useState<string>('');
+  const [spoilerContent, setSpoilerContent] = useState<string>('');
   const [addSpoilers, setAddSpoilers] = useState<boolean>(false);
   const titleField = useInputField({
     name: 'title',
-    placeholder: 'Title',
+    placeholder: 'Your title...',
     rules: {
       minLength: 3,
       maxLength: 75,
@@ -20,16 +41,38 @@ const UserReviewForm = (): JSX.Element | null => {
     },
   });
   return (
-    <form className="pl-2">
+    <form className="pl-2 flex flex-col">
+      <Section>
+        {'Reviewing '}
+        <div className="font-normal italic">
+          {media.name} {'('}
+          {mediaTypeToDisplayName(media.mediaType)}
+          {')'}
+        </div>
+        <Dropdown
+          options={[...seasonNames, 'Full show']}
+          defaultValue={'Full show'}
+        />
+      </Section>
+      <Section>
+        {'Title your review'}
+        <span className="font-normal italic">{' (required)'}</span>
+      </Section>
       <InputField {...titleField.getProps()} className={'h-8 w-3xs'} />
-      <h2>{'Your spoiler-free review'}</h2>
+      <Section>
+        {'Write a review'}
+        <span className="font-normal italic">{' (without spoilers)'}</span>
+        <div className="text-sm font-normal">
+          {`All users can see this, so don't spoil the plot!`}
+        </div>
+      </Section>
       <textarea
-        className="w-full pl-1 py-0.5 pr-2 ring-cyan-400 border-none rounded ring-1 bg-white text-gray-800 text-sm hover:ring-amber-300 focus:outline-none focus:ring-2 focus:ring-starblue"
+        className="w-full pl-1 py-0.5 pr-2 ring-cyan-400 border-none rounded ring-1 bg-white text-gray-800 text-sm hover:ring-amber-300 focus:outline-none focus:ring-2 focus:ring-starblue min-h-30"
         value={mainTextContent}
-        placeholder={'Your review...'}
+        placeholder={'Your spoiler-free review...'}
         onChange={(e) => setMainTextContent(e.target.value)}
       />
-      {'Add a Spoiler section?'}
+      <Section>{'Add spoiler section?'}</Section>
       <div className="flex flex-row gap-3">
         <span className="flex flex-row gap-2">
           {'Yes'}
@@ -42,7 +85,7 @@ const UserReviewForm = (): JSX.Element | null => {
           />
         </span>
         <span className="flex flex-row gap-2">
-          {'No'}{' '}
+          {'No'}
           <input
             type="radio"
             name="spoiler"
@@ -52,8 +95,43 @@ const UserReviewForm = (): JSX.Element | null => {
           />
         </span>
       </div>
+      {addSpoilers && (
+        <>
+          <Section>
+            {'Spoilers'}
+            <div className="text-sm font-normal">
+              {'You can write freely here, users will be warned!'}
+            </div>
+          </Section>
+          <textarea
+            className="w-full pl-1 py-0.5 pr-2 ring-cyan-400 border-none rounded ring-1 bg-white text-gray-800 text-sm hover:ring-amber-300 focus:outline-none focus:ring-2 focus:ring-starblue min-h-30"
+            value={spoilerContent}
+            placeholder={'Your spoiler review...'}
+            onChange={(e) => setSpoilerContent(e.target.value)}
+          />
+        </>
+      )}
+      <AnimatedDiv
+        animKey="submit-user-review-button"
+        className="flex justify-center"
+      >
+        <Button
+          disabled={!titleField.value || !mainTextContent}
+          type="submit"
+          className="relative mt-2 w-32 justify-center pl-5"
+        >
+          {'Send review'}
+          <span className="absolute w-full ">
+            <IconCreate width={16} />
+          </span>
+        </Button>
+      </AnimatedDiv>
     </form>
   );
+};
+
+const Section = ({ children }: PropsWithChildren): JSX.Element => {
+  return <h2 className="pt-3 pb-1 font-semibold">{children}</h2>;
 };
 
 export default UserReviewForm;
